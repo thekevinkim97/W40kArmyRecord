fb1d0b5ef5bda58023483e37231c8bd7f2093339
Kevin Kim <TheKevinKim97@gmail.com>
2020-05-13 18:54:10 -0400

    Initial template using Spring Framework

---
+++ src/main/java/com/kevin/W40kArmyRecord/W40kArmyRecordApplication.java
@@ -0,0 +1,13 @@
+package com.kevin.W40kArmyRecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W40kArmyRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W40kArmyRecordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kevin/W40kArmyRecord/W40kArmyRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevin.W40kArmyRecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W40kArmyRecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
